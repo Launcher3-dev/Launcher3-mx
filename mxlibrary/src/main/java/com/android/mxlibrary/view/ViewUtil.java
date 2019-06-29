a9d68486db9dd83a14df462e6a3c493c65a244d7
yuchuangu85 <yuchuangu85@gmail.com>
2019-06-29 20:47:49 +0800

    修改View滑动过程中是否显示

--- mxlibrary/src/main/java/com/android/mxlibrary/view/ViewUtil.java
+++ mxlibrary/src/main/java/com/android/mxlibrary/view/ViewUtil.java
@@ -7,19 +7,38 @@ public final class ViewUtil {
 
     /**
      * add by codemx
-     * 判断是否滑动到屏幕中
-     *
+     * 判断是否滑动(上下滑动)到屏幕中
+     * <p>
      * 在onScrollChanged方法中调用
      *
      * @param view 视图
+     *
      * @return 是否显示在屏幕中
      */
-    public static boolean isViewScrollToScreen(View view) {
+    public static boolean isViewVerticalScrollToScreen(View view) {
         Rect rect = new Rect();
         view.getLocalVisibleRect(rect);
         int top = rect.top;
         int bottom = rect.bottom;
         return top >= 0 && bottom > 0 && top < bottom;
+    }
+
+    /**
+     * add by codemx
+     * 判断是否滑动(上下滑动)到屏幕中
+     * <p>
+     * 在onScrollChanged方法中调用
+     *
+     * @param view 视图
+     *
+     * @return 是否显示在屏幕中
+     */
+    public static boolean isViewHorizontalScrollToScreen(View view) {
+        Rect rect = new Rect();
+        view.getLocalVisibleRect(rect);
+        int left = rect.left;
+        int right = rect.right;
+        return left >= 0 && right > 0 && left < right;
     }
 
 }
